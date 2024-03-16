62b101b8a053c1c77adedb01a0765da6a52f7fd6
Lucas Alencar <lucas130333@gmail.com>
2024-03-16 14:25:54 -0300

    Integração do projeto

---
+++ Projeto/getCliente.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET')/*So ira permitir GET*/{
+
+    $token = $_GET['token'];
+    $clienteID = $_GET['clienteID'];
+    echo $token;
+
+    if ($token == "daazi2024"){
+        ##Lé somente uma vez
+        include_once "db_modelo.php";
+        ## $variavel 
+        $statemente = $pdo->prepare("SELECT * FROM cliente WHERE id = ?");
+        $statemente->bindParam(1, $clienteID);
+        ##Manda execultar a valor da variavel como um comando SQL
+        $statemente->execute();
+
+        /*fetchAll -> desassocia os valores da variavel em um array*/
+        $results = $statemente->fetchAll(PDO::FETCH_ASSOC);
+        // echo "<pre>".print_r($results)."</pre";
+        $json = json_encode($results); /*Retorna um json*/
+echo $json;
+    }else{
+        echo "Não autorizado";
+    }
+
+}else{
+    echo "Acesso Negado";
+}
